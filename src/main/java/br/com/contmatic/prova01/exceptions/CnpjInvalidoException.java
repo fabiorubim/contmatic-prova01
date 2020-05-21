ff95e96a92ead92bad3ba53a757d315185aae750
Fabio Biasi Mello Rubim <fabiorubim@hotmail.com>
2020-05-21 16:04:13 -0300

    Creating a tag

--- src/main/java/br/com/contmatic/prova01/exceptions/CnpjInvalidoException.java
+++ src/main/java/br/com/contmatic/prova01/exceptions/CnpjInvalidoException.java
@@ -1,7 +1,12 @@
 package br.com.contmatic.prova01.exceptions;
 
 public class CnpjInvalidoException extends RuntimeException {
-	public CnpjInvalidoException() {
+	/**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    public CnpjInvalidoException() {
 		super("CNPJ inválido!");
 	}
 
--- src/test/java/br/com/contmatic/prova01/model/DepartamentoTest.java
+++ src/test/java/br/com/contmatic/prova01/model/DepartamentoTest.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -19,6 +20,11 @@ public class DepartamentoTest {
 	@BeforeAll
 	public static void setUp() {
 		departamento = new Departamento("RH");		
+	}
+	
+	@BeforeEach
+	public void setUpBefore() {
+		departamento.setNome("RH");
 	}
 	
 	@Test
@@ -47,54 +53,62 @@ public class DepartamentoTest {
 	}
 	
 	@Test
+	@Order(5)
 	public void deve_comparar_dois_objetos_com_hascode_iguais() {
 		Departamento novoDepartamento = new Departamento("TI");
 		assertEquals(departamento.hashCode(), novoDepartamento.hashCode());
 	}
 	
 	@Test
+	@Order(6)
 	public void deve_comparar_dois_objetos_com_hascode_diferentes_nome_nulo() {
 		Departamento novoDepartamento = new Departamento(null);
 		assertNotEquals(departamento.hashCode(), novoDepartamento.hashCode());
 	}
 	
 	@Test
+	@Order(7)
 	public void deve_comparar_dois_objetos_iguais() {
 		Departamento novoDepartamento = new Departamento("TI");
 		assertTrue(departamento.equals(novoDepartamento));
 	}
 	
 	@Test
+	@Order(8)
 	public void deve_passar_no_teste_se_os_objetos_forem_o_mesmo() {
 		assertTrue(departamento.equals(departamento));
 	}
 	
 	@Test
+	@Order(9)
 	public void deve_passar_note_teste_se_um_dos_objetos_for_nulo() {
 		Departamento novoDepartamento = null;
 		assertFalse(departamento.equals(novoDepartamento));
 	}
 	
 	@Test
+	@Order(10)
 	public void deve_passar_no_teste_se_os_objetos_foram_instancias_de_classe_diferentes() {
 		Endereco endereco = new Endereco("Rua Padre Estevão Pernet", "215", "São Paulo", "SP", "03315-000");
 		assertFalse(departamento.equals(endereco));
 	}
 	
 	@Test
+	@Order(11)
 	public void deve_comparar_tostring_de_dois_objetos() {
 		Departamento novoDepartamento = new Departamento("TI");
 		assertTrue(departamento.toString().equals(novoDepartamento.toString()));
 	}
 	
 	@Test
+	@Order(12)
 	public void deve_passar_no_teste_caso_a_nome_do_departamento1__seja_nulo_e_o_do_outro_objeto_nao_seja_nulo() {
 		Departamento departamento1 = new Departamento(null);
 		Departamento departamento2 = new Departamento("TI");
 		assertFalse(departamento1.equals(departamento2));
 	}
 	
-	@Test
+	@Test@Order(13)
 	public void deve_passar_no_teste_caso_o_nome_do_departamento1__seja_diferente_do_outro_objeto() {
 		Departamento departamento1 = new Departamento("RH");
 		Departamento departamento2 = new Departamento("TI");
